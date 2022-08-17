aa18e41c4c734f8b0f28047a95e63a022ccc0360
Christopher Marzulli <chriszulli15@gmail.com>
2022-08-17 16:39:05 -0400

    commit tree 25d90a9898e5576264b5846ab85e287e7961cc64

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
